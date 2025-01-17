00581072ac9dc1883362c5b5566c954167ccbb73
uzwal01 <uzwalduwal07@gmail.com>
2025-01-17 14:45:10 +0545

    fetch data from tmdb api in the page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
+const API_KEY = process.env.API_KEY;
+
+import Results from '@/components/Results';
 import React from 'react'
 
-function page() {
+async function page({searchParams}) {
+  const genre = searchParams.genre || 'fetchTrending'
+  const response = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`
+  )
+  const data = await response.json();
+  if(!response.ok){
+    throw new Error('Failed to fetch data')
+  }
+  const results = data.results;
+  console.log(results);
+  
   return (
     <div>
-      
+      <Results results={results}/>
     </div>
   )
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Results({results}) {
+  return (
+    <>
+    <div>
+      {
+        results.map((result)=> (
+         <div key={result.id}>
+            <h2>{result.original_title}</h2>
+         </div>   
+        ))
+      }
+    </div>
+    </>
+    
+  )
+}
+
+export default Results
