894e6fb2aa07a18023464392b2a857a9f1cdb6f8
uzwal01 <uzwalduwal07@gmail.com>
2025-01-17 15:05:42 +0545

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,8 @@ async function page({searchParams}) {
   const genre = searchParams.genre || 'fetchTrending'
   const response = await fetch(
     `https://api.themoviedb.org/3${
-      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`,
+      {next: {revalidate: 10000}}
   )
   const data = await response.json();
   if(!response.ok){
